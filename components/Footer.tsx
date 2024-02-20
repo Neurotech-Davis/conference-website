e02ab03c72093c1cc8dfb2fe56b25c74b97c120e
Dhruv Sangamwar <dsangamwar@gmail.com>
2024-02-20 13:29:21 -0800

    Added Footer

---
+++ components/Footer.tsx
@@ -0,0 +1,43 @@
+'use client';
+import React from "react";import {usePathname} from "next/navigation"
+import Link from "next/link"
+import Image from "next/image";
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import EmailIcon from '@mui/icons-material/Email';
+import InstagramIcon from '@mui/icons-material/Instagram';
+
+export default function Footer() {
+    return (
+        <>
+            <footer className="bg-blue-900">
+                <div className="flex flex-row justify-between x-auto w-full p-4 py-6 lg:py-8">
+
+                    <div className="flex flex-col items-start h-fullr">
+                        <span className="text-xs text-white">Made by Neurotech@Davis</span>
+                        <span className="text-xs text-white">Designed by Ciel Wood, Dhruv Sangamwar, and Kyle</span>
+                    </div>
+
+
+                    <div className="flex flex-row justify-between">
+                        
+                        <button className="flex px-4 flex-col items-center text-xs text-white ">
+                            <LinkedInIcon/>
+                            linkedIn
+                        </button>
+
+                        <button className="flex px-4 flex-col items-center text-xs text-white">
+                            <EmailIcon/>
+                            Email
+                        </button>
+
+                        <button className="flex px-4 flex-col items-center text-xs text-white">
+                            <InstagramIcon/>
+                            Instagram
+                        </button>
+
+                    </div>
+                </div>
+            </footer>
+        </>
+    )
+}
