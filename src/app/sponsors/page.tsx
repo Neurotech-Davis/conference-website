e395e2a6fd6f607b63a96592a7a078c824f16a12
Mateo Umaguing <maumaguing@gmail.com>
2024-04-22 17:09:50 -0700

    updated panelists and sponsors

--- src/app/sponsors/page.tsx
+++ src/app/sponsors/page.tsx
@@ -1,42 +1,58 @@
 import Image from 'next/image';
-import ucla_brain from '../../../public/ucla_brain.jpeg'
-import sponsor_2 from '../../../public/sponsor_2.jpeg'
+import uclabri from '../../../public/sponsors/bri.png'
+import davisne from '../../../public/sponsors/davis_neuroeng.png'
+import careai from '../../../public/sponsors/careai.png'
+import mbraintrain from '../../../public/sponsors/mbraintrain.png'
+import uclatnt from '../../../public/sponsors/ucla_tnt.png'
+import ntmc from '../../../public/sponsors/ntmc.png'
 
 
 export default function Members() {
   return (
     <main>
-      <div className="py-60 flex flex-col min-h-screen p-10">
-        <div className=" flex flex-col pt-8 items-center">
-          <Image className="rounded-md"
-            src={ucla_brain}
-            width={300}
-            height={300}
-            alt="Logo">
+      <div className="py-32 flex flex-col min-h-screen p-10">
+        <div className="flex flex-col pt-8 items-center">
+          <Image className = "p-10"
+            src={uclabri}
+            width={400}
+            height={250}
+            alt="UCLA BRI">
           </Image>
-          <div className="flex flex-col p-8 items-center">
-            <span className="font-semibold text-pretty text-lg">
-              UCLA Training in Neurotechnology Translation
-            </span>
-            {/* <div className=" flex">
-                  Amount of money
-                </div> */}
-          </div>
 
-          <Image className="rounded-md pt-10"
-            src={sponsor_2}
-            width={500}
-            height={500}
-            alt="Logo">
+          <Image className = "p-10"
+            src={davisne}
+            width={600}
+            height={250}
+            alt="Davis Neuroengineering">
           </Image>
-          <div className=" flex flex-col p-8 items-center">
-          <span className="font-semibold text-pretty text-lg">
-              UC Davis Center for Neuroengineering and Medicine
-            </span>
-            {/* <div className=" flex">
-                  Amount of money
-                </div> */}
-          </div>
+
+          <Image className = "p-10"
+            src={careai}
+            width={400}
+            height={250}
+            alt="care.ai">
+          </Image>
+
+          <Image className = "p-10"
+            src={mbraintrain}
+            width={400}
+            height={250}
+            alt="mBrainTrain">
+          </Image>
+
+          <Image className = "p-10"
+            src={ntmc}
+            width={400}
+            height={250}
+            alt="NTMC">
+          </Image>
+
+          <Image className = "p-10"
+            src={uclatnt}
+            width={600}
+            height={250}
+            alt="UCLA TNT">
+          </Image>
 
         </div>
       </div>
